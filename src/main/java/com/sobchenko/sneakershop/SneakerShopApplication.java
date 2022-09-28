cd92e28ba38d16131a353d6ae813665218b600eb
Sergey Sobchenko <ssobchenko@gmail.com>
2022-09-28 20:54:04 +0300

    Initial commit

---
+++ src/main/java/com/sobchenko/sneakershop/SneakerShopApplication.java
@@ -0,0 +1,14 @@
+package com.sobchenko.sneakershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SneakerShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobchenko/sneakershop/SneakerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobchenko.sneakershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
