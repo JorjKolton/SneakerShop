3cbe0e218c8507c8f7eef1f4be16e97b0073edcb
Sergey Sobchenko <ssobchenko@gmail.com>
2022-10-28 00:53:55 +0300

    add heroku config 22

--- src/main/java/com/sobchenko/sneakershop/service/UserService.java
+++ src/main/java/com/sobchenko/sneakershop/service/UserService.java
@@ -6,6 +6,7 @@ import com.sobchenko.sneakershop.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,7 +26,7 @@ public class UserService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
